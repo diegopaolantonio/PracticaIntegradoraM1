4dd185f9ba21ca9bf357201caec8b81ca77d5598
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2023-04-06 22:40:05 -0300

    Commit 2 - Correccion de erroes y continuacion de la base del programa

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 import express from "express";
-import messagesRouter from "./routes/messagesRouter.js";
-import viewsRouter from "./routes/viewsRouter.js"
+import handlebars from "express-handlebars";
+import messagesRouter from "./routes/messages.router.js";
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
+import viewsRouter from "./routes/views.router.js"
 import __dirname from "./utils.js"
 
 const app = express();
 
 app.use(express.json());
 
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
 app.listen(8080, () => {
     console.log("Server on port 8080");
 });
 
 app.use("/api/messages", messagesRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
